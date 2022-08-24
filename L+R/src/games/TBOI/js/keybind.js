5249fc0e7ecc6876f3dcd566ae775f63b1bef511
loss23 <mebob248888@gmail.com>
2022-08-24 17:11:38 -0500

    Updating UI and Games

---
+++ L+R/src/games/TBOI/js/keybind.js
@@ -0,0 +1,74 @@
+function keyboardEvent(){
+	//Gestion des touches du clavier
+	window.addEventListener("keydown", onKeyDown, false);
+	window.addEventListener("keyup", onKeyUp, false);}
+
+var keyW = false, keyA = false, keyS = false, keyD = false, keyQ = false, keyE = false; //WASD
+var keyLeft = false, keyUp = false, keyRight = false, keyDown = false; //ARROWS
+var keyPause = false, keyShift = false, keySpace = false;// ENTER
+
+var fullscreenAvailable = typeof screenfull !== 'undefined';
+if (fullscreenAvailable) {
+	screenfull.on('change', () => {
+		setTimeout(() => {
+			var elem = getEl('game');
+			const height = window.innerHeight
+			|| document.documentElement.clientHeight
+			|| document.body.clientHeight;
+			
+			if (screenfull.isFullscreen) {
+				elem.style.width = (height * 1.385) + 'px';
+			} else {
+				elem.style.height = '';
+				elem.style.width = '';
+			}
+		}, 100);
+	});
+}
+
+function toggleFullscreen() {
+	if (fullscreenAvailable) {
+		var elem = getEl('fullscreen');
+		screenfull.toggle(elem);
+	}
+}
+
+function onKeyDown(event){
+	var keyCode = event.keyCode;
+	switch(keyCode){
+		case 32: keySpace = true;event.preventDefault(); break;		// Space Bar
+		case 37: keyLeft = true;event.preventDefault(); break;		// Left arrow
+		case 38: keyUp = true;event.preventDefault(); break;		// up arrow
+		case 39: keyRight = true;event.preventDefault(); break; 	// right arrow
+		case 40: keyDown = true;event.preventDefault(); break; 	// down arrow
+		case 68: keyD = true; break; 		//d
+		case 83: keyS = true; break; 		//s
+		case 65: keyA = true; break; 		//a
+		case 87: keyW = true; break;		//w
+		case 81: keyQ = true; break;		//q
+		case 69: keyE = true; break;		//e
+		case 16: keyShift = true; break;	//shift
+		case 13: keyPause = true; break;	//enter
+		case 70: toggleFullscreen(); break; // f
+	}
+}
+function onKeyUp(event){
+	var keyCode = event.keyCode
+	switch(keyCode){
+		case 32: keySpace = false;event.preventDefault(); break;		// Space Bar
+		case 37: keyLeft = false; break; 	// Left arrow
+		case 38: keyUp = false; break; 		// up arrow
+		case 39: keyRight = false; break; 	// right arrow
+		case 40: keyDown = false; break; 	// down arrow
+		case 68: keyD = false; break; 		//d
+		case 83: keyS = false; break;		//s
+		case 65: keyA = false; break; 		//a
+		case 87: keyW = false; break; 		//w
+		case 81: keyQ = false; break;		//q
+		case 69: keyE = false; break;		//q
+		case 16: keyShift = false; break;	//shift
+		case 13: keyPause = false; break; 	//enter
+	}
+}
+
+console.log('keybind.js loaded');
---
+++ L+R/src/js/main.js
@@ -0,0 +1,25 @@
+/* Welcome to the main source of my website! This will be used to load all pages and load any js I make, unless it's a game! The games aren't usually made by me lmao. */
+
+function hov(t){
+    t.style = "border-radius: 5px; font-size: 25px; font-family: cursive; border-color:black; background-color:darkcyan; color:white;"
+}
+
+function unhov(t){
+    t.style = "border-radius: 5px; font-size: 25px; font-family: cursive; border-color:black; background-color:cyan; color:gray;"
+}
+
+function Tetris(){
+    window.location.href = "games/tetris.html"
+}
+
+function TBOI(){
+    window.location.href = "games/TBOI/TBOI.html"
+}
+
+function CC(){
+    window.location.href = "games/CC/CC.html"
+}
+
+function BC(){
+    window.location.href = "games/BC/BC.html"
+}
