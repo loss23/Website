e37b37bc0575eb00d13bc58d90064cf1c8d832fe
loss23 <61843498+loss23@users.noreply.github.com>
2022-08-25 09:05:12 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 /* Welcome to the main source of my website! This will be used to load all pages and load any js I make, unless it's a game! The games aren't usually made by me lmao. */
 
 function hov(t){
-    t.style = "border-radius: 5px; font-size: 25px; font-family: cursive; border-color:black; background-color:darkcyan; color:white;"
+    t.style = "border-radius: 5px; font-size: 25px; font-family: cursive; border-color:black; background-color:violet; color:white;"
 }
 
 function unhov(t){
-    t.style = "border-radius: 5px; font-size: 25px; font-family: cursive; border-color:black; background-color:cyan; color:gray;"
+    t.style = "border-radius: 5px; font-size: 25px; font-family: cursive; border-color:black; background-color:violet; color:gray;"
 }
 
 function Tetris(){
